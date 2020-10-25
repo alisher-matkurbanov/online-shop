0dd20ff51a62ababf318281b91343e95bf9bec2e
Alisher Matkurbanov <amatkurbanov@cloudlinux.com>
2020-10-25 23:41:40 +0500

    Change database to postgres

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'online_shop',
+        'NAME': 'online_shop',
+        'PASSWORD': 'online_shop',
+        'TEST': {
+            'NAME': 'test-db',
+        },
     }
 }
 
